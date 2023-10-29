073cea48eda29969387e73d5e0d6c75e5fcba8ab
Aliaa Nasser <125163801+AliaaNasser7@users.noreply.github.com>
2023-10-29 14:38:12 +0200

    Add files via upload

---
+++ Confirmation Rate.sql
@@ -0,0 +1,8 @@
+SELECT s.user_id ,
+       CASE WHEN c.time_stamp IS NULL THEN 0.00 ELSE  ROUND(SUM(action = 'confirmed')/COUNT(*),2) END AS 'confirmation_rate'
+FROM 
+       Signups s LEFT JOIN Confirmations c 
+ON 
+       c.user_id = s.user_id
+GROUP BY 
+       s.user_id
