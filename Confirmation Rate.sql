d91f2970aa136836794a77dffc264dd01b826c81
Aliaa Nasser <125163801+AliaaNasser7@users.noreply.github.com>
2023-12-06 15:21:40 +0200

    Update Confirmation Rate.sql

--- Confirmation Rate.sql
+++ Confirmation Rate.sql
@@ -6,3 +6,14 @@ ON
        c.user_id = s.user_id
 GROUP BY 
        s.user_id
+
+//Another Way//
+       
+SELECT s.user_id ,
+       ROUND(AVG(if(c.action = 'confirmed',1,0)),2)  AS 'confirmation_rate'
+FROM 
+       Signups s LEFT JOIN Confirmations c 
+ON 
+       c.user_id = s.user_id
+GROUP BY 
+       s.user_id
